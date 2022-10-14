624f0eb93fadc09bdf44be6c5d8c787e1b8c226c
russelibale808 <russelibale808@gmail.com>
2022-10-14 10:53:36 -1000

    variable lessons

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,52 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variables Lessons
+
+            //Declaring variables
+
+            double turtleWeight;
+
+            int turtleAge;
+
+            string turtleName;
+
+            string turtleLastName;
+
+            char turtleLegs;
+
+            decimal turtleSize;
+
+            bool myTurtle;
+
+            //Initializing variables
+
+            turtleWeight = 8.5;
+
+            turtleAge = 8;
+
+            turtleName = "Bobo";
+
+            turtleLastName = "Chomper";
+
+            turtleLegs = '3';
+
+            turtleSize = 16.25m;
+
+            myTurtle = true;
+
+            //Declard & Initialized at the same time
+
+            string fullName = turtleName + " " + turtleLastName;
+
+            string weightAndLength = turtleWeight + " and is " + turtleSize;
+
+            string lazy = "Is my turle active or lazy? True or False?. " + myTurtle;
+
+            Console.WriteLine($"My turtles name is {fullName}, He is {turtleAge} years old." +
+                              $" He weighs {weightAndLength} in length." +
+                              $" He also has {turtleLegs} legs." +
+                              $" {lazy}, my turtle is lazy.");
         }
     }
 }
